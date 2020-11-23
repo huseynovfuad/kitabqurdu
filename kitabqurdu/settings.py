d15121cb762a3856245795aa58240a3fc89c1bfd
huseynovfuad <fhuseynov803@gmail.com>
2020-11-23 12:54:24 +0400

    debug false

--- kitabqurdu/settings.py
+++ kitabqurdu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uj@^45i(tgvt5t98d%jrwd)gtstlx^-!a#of!mu#k4+(f!ko1*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
